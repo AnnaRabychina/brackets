56d5c2f2fb7455147e2987472fa01dd2e9c85e98
AnnaRabychina <nutochka@tut.by>
2021-08-28 16:25:16 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let config =Object.fromEntries(bracketsConfig);
+  let stack = [];
+    for (i = 0; i < str.length; i++){
+      let element = str[i];
+      let topStack = stack[stack.length-1];
+      if (topStack !== undefined && config[topStack] === element){
+        stack.pop();
+      } else if (element in config){
+        stack.push(element);
+      } else if(stack.length === 0){
+        return false;
+      }
+    }
+  
+    if (stack.length === 0){
+      return  true;
+    } else {
+       return false;
+    }
 }
+
